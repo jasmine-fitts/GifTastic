844c8ac1918b9751efe64973a712abd5b0dd12fd
Jasmine Fitts <jasminefitts@Jasmines-MBP.county.durham-nc.net>
2018-05-23 12:55:37 -0400

    updated index and js files

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//Create an array of strings, each related to a topic
+
+var topicArray =  ["beyonce", "rihanna", "normani", "pop"]
+
+//Save the array to a variable 'topics'
+
+//Choose a theme for the gifs
+
+//Take the topics in the array and create buttons in the HMTL
+ 
+//Use a fot loop that appends a button for each string in the array
+
+//When the user clicks on the button, the button should grad 10 non animated gif imagesfrom teh GIPHY API and place them on the page
+
+//When the user clicks on the still images the gif should animate --- when the use clicks on the giphy image again it will stop playing
+
+//Display under every gif its rating (PG, G, and so on)
